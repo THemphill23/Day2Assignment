3deb2aa47b5846c260243bbf641bca1ddf5c60d1
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-04 19:59:17 -0400

    Day 2 Assignment

---
+++ Day2Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day2Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
