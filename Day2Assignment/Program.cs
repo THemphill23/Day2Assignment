706d43ee606bfff3b0e02bd8b806790a40bc0387
Taylor Hemphill <hemphilltaylor@gmail.com>
2019-09-04 20:11:05 -0400

    Character Class and Name

--- Day2Assignment/Program.cs
+++ Day2Assignment/Program.cs
@@ -6,7 +6,14 @@ namespace Day2Assignment
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome Adventurer");
+            Console.WriteLine("Choose Your Class");
+            Console.WriteLine("Fighter Mage Rouge");
+            string className = Console.ReadLine();
+
+            Console.WriteLine("Add Character Name");
+            string charName = Console.ReadLine();
+            Console.WriteLine(className + " " + charName);
         }
     }
 }
